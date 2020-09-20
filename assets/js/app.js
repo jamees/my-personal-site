8800db6bdbf8eaf5852f86fcdc28f1d51acb159e
James Cortés <james.cortes.t@gmail.com>
2020-09-20 11:45:30 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,6 @@ var app = new Vue({
       titleOne: 'james_',
       titleTwo: 'dot',
       titleThree: '_cl',
-      subTitle: 'learning, coding and testing cool stuffs...'
+      subTitle: ''
     }
 });
