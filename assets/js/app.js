e1ca7e47dff65596c9c7d9d02a389128e91a85b8
James <james.cortes.t@gmail.com>
2024-05-22 06:06:12 -0400

    size navbaf and vuejs data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,22 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      titleOne: 'james',
-      titleTwo: '.cl',
-      titleThree: '',
-      subTitle: ''
-    }
+  el: '#app',
+  data: {
+    titleOne: 'james',
+    titleTwo: '.cl',
+    titleThree: '',
+    subTitle: ' ',
+    posts: [
+      {
+        img: { src: 'logo.png', alt: '' },
+        title: 'Logo',
+        desc: 'Este es mi nuevo logo. Estoy muy emocionado porque es parte de mi nueva marca personal y tener esto es como un nuevo comienzo con muchas cosas nuevas por venir.',
+        date: '2024-05-21',
+        source: '',
+        link: '',
+        type: '',
+        size: ''
+      },
+      {}
+    ]
+  }
 });
